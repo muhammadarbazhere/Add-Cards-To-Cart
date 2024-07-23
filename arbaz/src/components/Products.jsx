25fe9b6f3c1e5b7960a85290396a2f860e8754c3
Muhammad Arbaz <muhammad.arbazhere@gmail.com>
2024-07-23 18:05:02 +0500

    Initial commit

---
+++ arbaz/src/components/Products.jsx
@@ -0,0 +1,42 @@
+const products = [
+    {
+      id: 1,
+      title: 'Product 1',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laudantium, aut quibusdam aperiam itaque vero ullam eligendi libero corporis dolorum. Atque dicta cupiditate nostrum',
+      image: 'https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+      id: 2,
+      title: 'Product 2',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laudantium, aut quibusdam aperiam itaque vero ullam eligendi libero corporis dolorum. Atque dicta cupiditate nostrum',
+      image: 'https://images.pexels.com/photos/4495705/pexels-photo-4495705.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 3,
+        title: 'Product 3',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laudantium, aut quibusdam aperiam itaque vero ullam eligendi libero corporis dolorum. Atque dicta cupiditate nostrum',
+        image: 'https://images.pexels.com/photos/934070/pexels-photo-934070.jpeg?auto=compress&cs=tinysrgb&w=600'
+      },
+      {
+        id: 4,
+        title: 'Product 4',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laudantium, aut quibusdam aperiam itaque vero ullam eligendi libero corporis dolorum. Atque dicta cupiditate nostrum',
+        image: 'https://images.pexels.com/photos/1210484/pexels-photo-1210484.jpeg?auto=compress&cs=tinysrgb&w=600'
+      },
+      {
+        id: 5,
+        title: 'Product 5',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laudantium, aut quibusdam aperiam itaque vero ullam eligendi libero corporis dolorum. Atque dicta cupiditate nostrum',
+        image: 'https://images.pexels.com/photos/45055/pexels-photo-45055.jpeg?auto=compress&cs=tinysrgb&w=600'
+      },
+      {
+        id: 6,
+        title: 'Product 6',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laudantium, aut quibusdam aperiam itaque vero ullam eligendi libero corporis dolorum. Atque dicta cupiditate nostrum',
+        image: 'https://images.pexels.com/photos/3735641/pexels-photo-3735641.jpeg?auto=compress&cs=tinysrgb&w=600'
+      },
+    
+  ];
+  
+  export default products;
+  
---
+++ arbaz/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import CardView from '../components/CardView'
+import MyCardView from '../components/myCardView'
+
+
+function Home() {
+  return (
+    <div>
+
+
+ <MyCardView/> 
+
+<h1 style={{marginLeft:'130px'}}>Our Courses</h1>
+        <CardView />
+
+
+    </div>
+  )
+}
+
+export default Home
